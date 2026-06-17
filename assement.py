ef7988944c62cbeddba3d0d63e6258b7d8ac0e0a
harrynoesgaard-ui <harrynoesgaard@riccarton.school.nz>
2026-06-18 11:44:21 +1200

    rough end product

--- assement.py
+++ assement.py
@@ -35,6 +35,8 @@ def print_parameter_query(fields:str, where:str, parameter):
     print(tabulate(results,fields.split(",")))
     db.close()  
 
+
+"""Code for the menu interface"""
 choice =''
 while choice != 'Z':
     choice = input('Welcome to the steam locomotive database\n\n'
@@ -42,13 +44,30 @@ while choice != 'Z':
                      'A: Age and built date\n' 
                      'B: Whyte and boiler\n'
                      'C: Locomotive, nickname, and current owner\n'
-                     'Z: Exit the database ')
+                     'D: Built, Manufacture, and nickname\n' 
+                     "E: American locomotive Company Loco's\n" 
+                     "F: Baldwin Locomotive Works Loco's\n" 
+                     "G: Locomotives with boilers longer than 20 feet\n" 
+                     "H: Locomotives built before 1920\n" 
+                     "Z: Exit the database \n"
+                     "Type here: ")
     
-    if choice == 'A':
+    choice = choice.lower()
+    if choice == 'a':
         print_query('age_and_built_date')
-    elif choice == 'B':
+    elif choice == 'b':
         print_query('whyte_and_boiler')
-    elif choice == 'C':
+    elif choice == 'c':
         print_query('locomotive_owner')
+    elif choice == 'd':
+        print_query('nickname_manufactuer')
+    elif choice == 'e':
+        print_query('alco_locos')
+    elif choice == 'f':
+        print_query('baldwin_locos')
+    elif choice == 'g':
+        print_query('long_boiler')
+    elif choice == 'h':
+        print_query('old_locos')
     elif choice == 'Z':
         exit
